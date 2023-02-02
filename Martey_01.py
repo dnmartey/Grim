1078cac5f3766ac5f0f341012fadd9434d520568
dnmartey <daniel.martey@go.mnstate.edu>
2023-02-02 15:59:41 -0600

    All my new files

---
+++ Martey_01.py
@@ -0,0 +1,21 @@
+
+# Daniel Martey
+#Due 09/02/2021
+#Minnesota State University Moorhead
+#CSIS 152
+#Assignment 01
+
+a=1
+b=2
+c=3
+d=4
+e=5
+f=6
+g=7
+h=8
+i=9
+j=10
+
+sum=a+b+c+d+e+f+g+h+i+j
+
+print(a,b,c,d,e,f,g,h,i,j,sum)
